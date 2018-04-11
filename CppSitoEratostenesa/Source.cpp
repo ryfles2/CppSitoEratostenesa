16b2bf27a5fc6ee5954d8873386f29dcbbac3938
ryfles2 <rafal.polanin@unilodz.eu>
2018-04-11 15:19:35 +0200

    add files

---
+++ CppSitoEratostenesa/Source.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+bool czy_pierwsza(int n)
+{
+	if (n<2) return false; 
+
+	for (int i = 2; i*i <= n; i++)
+	{
+		if (n%i == 0) return false;
+	}
+	return true;
+}
+
+int main()
+{
+	int n;
+
+	cout << "Podaj liczbę: ";
+	cin >> n;
+
+	if (czy_pierwsza(n))  cout << "Liczba " << n << " jest pierwsza" << endl;
+	else cout << "Liczba " << n << " nie jest pierwsza" << endl;
+
+	return 0;
+}
